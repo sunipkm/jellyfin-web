1e1afc2c96b6549988bdfe26c3d5fa758dc9d942
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2022-01-21 18:14:00 -0500

    Jellyfin Web with custom resolution settings v10.7.7

---
+++ fedora/jellyfin-web.spec
@@ -0,0 +1,68 @@
+%global         debug_package %{nil}
+
+Name:           jellyfin-web
+Version:        10.7.7
+Release:        1%{?dist}
+Summary:        The Free Software Media System web client
+License:        GPLv3
+URL:            https://jellyfin.org
+# Jellyfin Server tarball created by `make -f .copr/Makefile srpm`, real URL ends with `v%%{version}.tar.gz`
+Source0:        jellyfin-web-%{version}.tar.gz
+
+%if 0%{?centos}
+BuildRequires:  yarn
+%else
+BuildRequires:  nodejs-yarn
+%endif
+# sadly the yarn RPM at https://dl.yarnpkg.com/rpm/ uses git but doesn't Requires: it
+# ditto for Fedora's yarn RPM
+BuildRequires: git
+BuildArch:		noarch
+%if 0%{?fedora} >= 33
+BuildRequires: nodejs
+%endif
+
+# Disable Automatic Dependency Processing
+AutoReqProv:    no
+
+%description
+Jellyfin is a free software media system that puts you in control of managing and streaming your media.
+
+
+%prep
+%autosetup -n jellyfin-web-%{version} -b 0
+
+%build
+
+%install
+yarn install
+%{__mkdir} -p %{buildroot}%{_datadir}
+mv dist %{buildroot}%{_datadir}/jellyfin-web
+%{__install} -D -m 0644 LICENSE %{buildroot}%{_datadir}/licenses/jellyfin/LICENSE
+
+%files
+%defattr(644,root,root,755)
+%{_datadir}/jellyfin-web
+%{_datadir}/licenses/jellyfin/LICENSE
+
+%changelog
+* Sun Sep 05 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.7; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.7
+* Thu May 20 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.6; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.6
+* Tue May 04 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.5; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.5
+* Tue May 04 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.4; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.4
+* Tue May 04 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.3; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.3
+* Sun Apr 11 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.2; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.2
+* Sun Mar 21 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New upstream version 10.7.1; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.1
+* Mon Mar 08 2021 Jellyfin Packaging Team <packaging@jellyfin.org>
+- New stable release 10.7.0; release changelog at https://github.com/jellyfin/jellyfin-web/releases/tag/v10.7.0
+* Mon Jul 27 2020 Jellyfin Packaging Team <packaging@jellyfin.org>
+- Forthcoming stable release
+* Mon Mar 23 2020 Jellyfin Packaging Team <packaging@jellyfin.org>
+- Forthcoming stable release
---
+++ src/components/qualityOptions.js
@@ -0,0 +1,127 @@
+import globalize from '../scripts/globalize';
+
+export function getVideoQualityOptions(options) {
+    const maxStreamingBitrate = options.currentMaxBitrate;
+    let videoWidth = options.videoWidth;
+    const videoHeight = options.videoHeight;
+
+    // If the aspect ratio is less than 16/9 (1.77), set the width as if it were pillarboxed.
+    // 4:3 1440x1080 -> 1920x1080
+    if (videoWidth / videoHeight < 16 / 9) {
+        videoWidth = videoHeight * (16 / 9);
+    }
+
+    const maxAllowedWidth = videoWidth || 4096;
+
+    const qualityOptions = [];
+
+    const autoQualityOption = {
+        name: globalize.translate('Auto'),
+        bitrate: 0,
+        selected: options.isAutomaticBitrateEnabled
+    };
+
+    if (options.enableAuto) {
+        qualityOptions.push(autoQualityOption);
+    }
+
+    // Quality options are indexed by bitrate. If you must duplicate them, make sure each of them are unique (by making the last digit a 1)
+    if (maxAllowedWidth >= 3800) {
+        qualityOptions.push({ name: '4K - 8 Mbps', maxHeight: 2160, bitrate: 8000000 });
+        qualityOptions.push({ name: '4K - 6 Mbps', maxHeight: 2160, bitrate: 6000000 });
+    }
+    if (maxAllowedWidth >= 2500) {
+        qualityOptions.push({ name: '2K - 4 Mbps', maxHeight: 1440, bitrate: 4000000 });
+        qualityOptions.push({ name: '2K - 3.5 Mbps', maxHeight: 1440, bitrate: 3500000 });
+    }
+    // Some 1080- videos are reported as 1912?
+    if (maxAllowedWidth >= 1900) {
+        qualityOptions.push({ name: '1080p - 3 Mbps', maxHeight: 1080, bitrate: 3000000 });
+        qualityOptions.push({ name: '1080p - 2 Mbps', maxHeight: 1080, bitrate: 2000000 });
+    }
+    if (maxAllowedWidth >= 1260) {
+        qualityOptions.push({ name: '720p - 1.5 Mbps', maxHeight: 720, bitrate: 1500000 });
+        qualityOptions.push({ name: '720p - 1 Mbps', maxHeight: 720, bitrate: 1000000 });
+    }
+    if (maxAllowedWidth >= 620) {
+        qualityOptions.push({ name: '480p - 720 kbps', maxHeight: 480, bitrate: 750000 });
+        qualityOptions.push({ name: '480p - 420 kbps', maxHeight: 480, bitrate: 420001 });
+    }
+
+    qualityOptions.push({ name: '360p - 420 kbps', maxHeight: 360, bitrate: 420000 });
+
+    if (maxStreamingBitrate) {
+        let selectedIndex = qualityOptions.length - 1;
+        for (let i = 0, length = qualityOptions.length; i < length; i++) {
+            const option = qualityOptions[i];
+
+            if (option.bitrate > 0 && option.bitrate <= maxStreamingBitrate) {
+                selectedIndex = i;
+                break;
+            }
+        }
+
+        const currentQualityOption = qualityOptions[selectedIndex];
+
+        if (!options.isAutomaticBitrateEnabled) {
+            currentQualityOption.selected = true;
+        } else {
+            autoQualityOption.autoText = currentQualityOption.name;
+        }
+    }
+
+    return qualityOptions;
+}
+
+export function getAudioQualityOptions(options) {
+    const maxStreamingBitrate = options.currentMaxBitrate;
+
+    const qualityOptions = [];
+
+    const autoQualityOption = {
+        name: globalize.translate('Auto'),
+        bitrate: 0,
+        selected: options.isAutomaticBitrateEnabled
+    };
+
+    if (options.enableAuto) {
+        qualityOptions.push(autoQualityOption);
+    }
+
+    qualityOptions.push({ name: '2 Mbps', bitrate: 2000000 });
+    qualityOptions.push({ name: '1.5 Mbps', bitrate: 1500000 });
+    qualityOptions.push({ name: '1 Mbps', bitrate: 1000000 });
+    qualityOptions.push({ name: '320 kbps', bitrate: 320000 });
+    qualityOptions.push({ name: '256 kbps', bitrate: 256000 });
+    qualityOptions.push({ name: '192 kbps', bitrate: 192000 });
+    qualityOptions.push({ name: '128 kbps', bitrate: 128000 });
+    qualityOptions.push({ name: '96 kbps', bitrate: 96000 });
+    qualityOptions.push({ name: '64 kbps', bitrate: 64000 });
+
+    if (maxStreamingBitrate) {
+        let selectedIndex = qualityOptions.length - 1;
+        for (let i = 0, length = qualityOptions.length; i < length; i++) {
+            const option = qualityOptions[i];
+
+            if (option.bitrate > 0 && option.bitrate <= maxStreamingBitrate) {
+                selectedIndex = i;
+                break;
+            }
+        }
+
+        const currentQualityOption = qualityOptions[selectedIndex];
+
+        if (!options.isAutomaticBitrateEnabled) {
+            currentQualityOption.selected = true;
+        } else {
+            autoQualityOption.autoText = currentQualityOption.name;
+        }
+    }
+
+    return qualityOptions;
+}
+
+export default {
+    getVideoQualityOptions,
+    getAudioQualityOptions
+};
